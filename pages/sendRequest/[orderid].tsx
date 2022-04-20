32f03fa3c9d95dc558d6f1ffc253c96ebfb7e0a1
alex-kisimov <aki@codehousegroup.com>
2022-04-20 14:10:50 +0100

    - Added edit button

--- pages/sendRequest/[orderid].tsx
+++ pages/sendRequest/[orderid].tsx
@@ -2,6 +2,7 @@
 /* eslint-disable radix */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { FunctionComponent, useEffect, useState } from 'react'
+import Link from 'next/link'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import {
@@ -63,6 +64,18 @@ const SendrequestPage: FunctionComponent = () => {
       <h1 className={styles.title}>Send request</h1>
       {orderDetails && (
         <>
+          <div className={styles.tableHead}>
+            <span>Order details</span>
+            <Link href={`/appointmentListing/${query.orderid.toString()}`}>
+              <a className={styles.edit}>
+                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <g fill="#FF6441" fillRule="evenodd">
+                    <path d="M13.523 1.546 2.917 12.153l-.529 3.71 3.71-.528L16.706 4.728l-3.182-3.182zm0 2.122 1.06 1.06-9.191 9.192-1.238.177.176-1.237 9.193-9.192zM9 15h9v1H9z" />
+                  </g>
+                </svg>
+              </a>
+            </Link>
+          </div>
           {orderDetails.map((lineItem, i) => {
             return (
               <div key={lineItem.ID}>

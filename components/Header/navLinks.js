2aa325ca02cd7c888365731382722202764c93db
CatiaGomes82 <cgo@codehousegroup.com>
2022-04-21 15:32:39 +0100

    add missing file

---
+++ components/Header/navLinks.js
@@ -0,0 +1,26 @@
+export const buyerLinks = [
+    {
+        Name: "Dashboard",
+        Url: '/',
+        IsDashboard: true
+    },
+    {
+        Name: "Single Service",
+        Url: "/singleService"
+    },
+    {
+        Name: "Appointment Listing",
+        Url: "/appointmentListing"
+    },
+    {
+        Name: "Contact Terminal",
+        Url: "/"
+    }
+];
+
+export const supplierLinks = [
+    {
+        Name: "Service Enquiries",
+        Url: "/orderCompletion"
+    }
+];

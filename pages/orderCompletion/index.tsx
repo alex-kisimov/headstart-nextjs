a14b4ad6878374100a340dc9bbdf6555a8c7ea19
Phm <phm@codehousegroup.com>
2022-04-25 18:46:22 +0100

    Table style results

--- pages/orderCompletion/index.tsx
+++ pages/orderCompletion/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { FunctionComponent, useEffect, useRef, useState } from 'react';
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
 import { BuyerProduct } from 'ordercloud-javascript-sdk';
 import styles from './orderCompletion.module.css';
 // import ProductCard from './ProductCard';
@@ -73,52 +73,118 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
                     )}
                     {!showLoader && (
                         <>
-                            {orders.map((order, i) => {
-                                return (
-                                    <div key={i}>
-                                        <hr />
-                                        Created by User: {order.FromUser.Username}
-                                        <br />
-                                        Company: {order.FromCompanyID}
-                                        <br />
-                                        Date submitted: {order.DateSubmitted}
-                                        {order.LineItems.map((lineItem, lineItemIndex) => {
-                                            return (
-                                                <div key={lineItemIndex}>
-                                                    <br />
-                                                    Service: {lineItem.Product.Name}
-                                                    <br />
-                                                    Service details:
-                                                    <ul>
-                                                        <li>Width: {lineItem?.xp?.CargoWidth}</li>
-                                                        <li>Height: {lineItem?.xp?.CargoHeight}</li>
-                                                        <li>Length: {lineItem?.xp?.CargoLenght}</li>
-                                                        <li>Weight: {lineItem?.xp?.CargoWeight}</li>
-                                                    </ul>
-                                                    <br />
-                                                </div>
-                                            )
-                                        })}
-                                        Total: {order.Total}
-                                        <br />
-                                        {order?.xp?.RequestToCancel && (order.Status !== 'Completed' && order.Status !== 'Canceled') && (
-                                            <div>
-                                                <p>User has requested cancellation of this service</p>
-                                                <button type="button" className="btn btn--secondary" data-orderid={order.ID} onClick={cancelOrder}>Cancel</button>
-                                            </div>
-                                        )}
-                                        {(order.Status !== 'Completed' && order.Status !== 'Canceled') && (
-                                            <button className="btn" data-orderid={order.ID} onClick={completeOrder}>Complete</button>
-                                        )}
-                                        {order.Status === 'Completed' && (
-                                            <div>Order completed</div>
-                                        )}
-                                        {order.Status === 'Canceled' && (
-                                            <div className={styles.cancelled}>Order cancelled</div>
-                                        )}
+                            <div className="generic-table__actions">
+                                <div className="generic-table__actions-feature">
+                                    <div className="today-only generic-table__action" data-key="TruckAppointmentDate">
+                                        <label className="today-only__label">
+                                            <input type="radio" name="today-only__radio" className="today-only__radio today-only__radio--all" onChange={() => console.log("I don't change")} checked={true}/>
+                                            All appointments
+                                        </label>
+                                        <label className="today-only__label">
+                                            <input type="radio" name="today-only__radio" className="today-only__radio today-only__radio--today"/>
+                                            Today only
+                                        </label>
                                     </div>
-                                )
-                            })}
+                                </div>
+                                <div className="generic-table__actions-secondary">
+                                    <button className="action generic-table__action generic-table__action--desktop generic-table__action--filter">
+                                        <span className="action__icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 22" width="26" height="22"><path d="M19 14a4.002 4.002 0 013.874 3H26v2h-3.126a4.002 4.002 0 01-7.748 0H0v-2h15.126c.444-1.725 2.01-3 3.874-3zm0 2a2 2 0 10-.001 3.999A2 2 0 0019 16zM9 7a4.002 4.002 0 013.874 3H26v2H12.874a4.002 4.002 0 01-7.748 0H0v-2h5.126C5.57 8.275 7.136 7 9 7zm0 2a2 2 0 10-.001 3.999A2 2 0 009 9zm6-9a4.002 4.002 0 013.874 3H26v2h-7.126a4.002 4.002 0 01-7.748 0H0V3h11.126c.444-1.725 2.01-3 3.874-3zm0 2a2 2 0 10-.001 3.999A2 2 0 0015 2z"></path></svg>
+                                        </span>
+                                        <span className="action__label">
+                                            Search and filter
+                                        </span>
+                                    </button>
+                                    <a href="/apm/api/tas/appointmentListingCsv?terminalId=ea5a1241-9736-4b82-b815-eae337ef1ac6&amp;fileName=appointment-listing-20210812Z%24.csv" className="action action--secondary action--reverse generic-table__action generic-table__action--desktop">
+                                        <span className="action__icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 18" width="14" height="18"><path d="M10.664 0H0v18h14V3.454zm-.849 2L12 4.263V16H2V2zM3 10h3v2H3zm0-3h8v2H3zm4 3h4v2H7zm-4 3h3v2H3zm4 0h4v2H7z" fill="#ff6441"></path></svg>
+                                        </span>
+                                        <span className="action__label">
+                                            Export to CSV
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <table className="fixed-table__data-table generic-table__table">
+                                <thead className="generic-table__thead">
+                                    <tr className="generic-table__row">
+                                        {["Created by User", "Company", "Date submitted", "Service", "Width", "Height", "Length", "Weight", "Total", "Status"].map((title, i) => {
+                                            return (
+                                                <th key={i} className="generic-table__header">
+                                                    <button className="generic-sort">
+                                                        <span>{title}</span>
+                                                        <span className="generic-sort__icon"></span>
+                                                    </button>
+                                                </th>
+                                            );
+                                        })}
+                                    </tr>
+                                </thead>
+                                <tbody className="generic-table__tbody">
+                            
+                                {orders.map((order, i) => {
+                                    const hasMultipleLineItems = order.LineItems.length > 1;
+                                    let liOrders;
+                                    if (hasMultipleLineItems) {
+                                        liOrders = order.LineItems.map((li, liIndex) => {
+                                            return {
+                                                username: liIndex === 0 ? order.FromUser.Username : "",
+                                                fromCompanyID: liIndex === 0 ? order.FromCompanyID : "",
+                                                dateSubmitted: liIndex === 0 ? order.DateSubmitted.split("T")[0] : "",
+                                                productName: li.Product.Name,
+                                                cargoWidth: li.xp?.CargoWidth,
+                                                cargoHeight: li.xp?.CargoHeight,
+                                                cargoLength: li.xp?.CargoLenght,
+                                                cargoWeight: li.xp?.CargoWeight,
+                                                total: liIndex === 0 ? order.Total : "",
+                                                status: liIndex === 0 ? order.Status : ""
+                                            }
+                                        });
+                                    }
+
+                                    return (
+                                        hasMultipleLineItems ? (
+                                            liOrders.map((li, ii) => {
+                                                return (
+                                                    <tr key={ii} className={`generic-table__row generic-table__row--${(i + 1) % 2 ? 'odd' : 'even'}`}>
+                                                        <td className="generic-table__cell">{li.username}</td>
+                                                        <td className="generic-table__cell">{li.fromCompanyID}</td>
+                                                        <td className="generic-table__cell">{li.dateSubmitted}</td>
+                                                        <td className="generic-table__cell">{li.productName}</td>
+                                                        <td className="generic-table__cell">{li.cargoWidth}</td>
+                                                        <td className="generic-table__cell">{li.cargoHeight}</td>
+                                                        <td className="generic-table__cell">{li.cargoLength}</td>
+                                                        <td className="generic-table__cell">{li.cargoWeight}</td>
+                                                        <td className="generic-table__cell">{li.total}</td>
+                                                        <td className="generic-table__cell">{li.status}</td>
+                                                    </tr>
+                                                );
+                                            })
+                                        ) : (
+                                            <tr key={i} className={`generic-table__row generic-table__row--${(i + 1) % 2 ? 'odd' : 'even'}`}>
+                                                <td className="generic-table__cell">{order.FromUser.Username}</td>
+                                                <td className="generic-table__cell">{order.FromCompanyID}</td>
+                                                <td className="generic-table__cell">{order.DateSubmitted}</td>
+                                                {order.LineItems.map((lineItem, lineItemIndex) => {
+                                                    return (
+                                                        <React.Fragment key={lineItemIndex}>
+                                                        <td className="generic-table__cell">{lineItem.Product.Name}</td>
+                                                        <td className="generic-table__cell">{lineItem?.xp?.CargoWidth}</td>
+                                                        <td className="generic-table__cell">{lineItem?.xp?.CargoHeight}</td>
+                                                        <td className="generic-table__cell">{lineItem?.xp?.CargoLenght}</td>
+                                                        <td className="generic-table__cell">{lineItem?.xp?.CargoWeight}</td>
+                                                        </React.Fragment>
+                                                    )
+                                                })}
+                                                <td className="generic-table__cell">{order.Total}</td>
+                                                <td className="generic-table__cell">{order.Status}</td>
+                                            </tr>
+                                        )
+                                    )
+                                })}
+                                </tbody>
+                            </table>
                         </>
                     )}
                 </div>

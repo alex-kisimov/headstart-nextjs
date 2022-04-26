b1fcbc67f268f972f6440571b657e4c0807ccdc9
Phm <phm@codehousegroup.com>
2022-04-26 14:21:31 +0100

    Update order status on action. Other CR changes

--- pages/orderCompletion/index.tsx
+++ pages/orderCompletion/index.tsx
@@ -48,15 +48,35 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
     }
 
     const completeOrder = (e) => {
+        const orderIndex = e.target.closest("tr").dataset.index;
+        const button = e.target.closest("button");
+        button.classList.add("action--loading");
+        button.setAttribute("disabled", true);
+
         Orders.Complete('Incoming', e.currentTarget.dataset.orderid).then((response) => {
-            console.log(response)
-        })
+            button.classList.remove("action--loading");
+            button.removeAttribute("disabled");
+
+            const newOrders = orders;
+            newOrders[orderIndex] = { ...newOrders[orderIndex], ...response};
+            setOrders([...newOrders]);
+        });
     }
 
     const cancelOrder = (e) => {
+        const orderIndex = e.target.closest("tr").dataset.index;
+        const button = e.target.closest("button");
+        button.classList.add("action--loading");
+        button.setAttribute("disabled", true);
+        
         Orders.Cancel('Incoming', e.currentTarget.dataset.orderid).then((response) => {
-            console.log(response)
-        })
+            button.classList.remove("action--loading");
+            button.removeAttribute("disabled");
+
+            const newOrders = orders;
+            newOrders[orderIndex] = { ...newOrders[orderIndex], ...response};
+            setOrders([...newOrders]);
+        });
     }
 
     useEffect(() => {
@@ -66,46 +86,20 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
     return (
         <div>
             <div className="wrapper">
-                <h1 className={styles.title}>Service Enquiries</h1>
+                <div className="title-striped title-striped--margin-top">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 497.6 116.7" width="447.6" height="116.7">
+                            <path d="M89 42.8v-8.3l62.4-28.6v11zM23.1 53.2V59l96.8-48.6V0zM0 79.6l53.4-22.1v-6.7L0 75.2z" fill="#FF6441"></path>
+                        </svg>
+                    </div>
+                    <h1>Service Enquiries</h1>
+                </div>
                 <div className={styles.results}>
                     {showLoader && (
                         <div className={styles.loader}><Loader /></div>
                     )}
                     {!showLoader && (
                         <>
-                            <div className="generic-table__actions">
-                                <div className="generic-table__actions-feature">
-                                    <div className="today-only generic-table__action" data-key="TruckAppointmentDate">
-                                        <label className="today-only__label">
-                                            <input type="radio" name="today-only__radio" className="today-only__radio today-only__radio--all" onChange={() => console.log("I don't change")} checked={true}/>
-                                            All appointments
-                                        </label>
-                                        <label className="today-only__label">
-                                            <input type="radio" name="today-only__radio" className="today-only__radio today-only__radio--today"/>
-                                            Today only
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="generic-table__actions-secondary">
-                                    <button className="action generic-table__action generic-table__action--desktop generic-table__action--filter">
-                                        <span className="action__icon">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 22" width="26" height="22"><path d="M19 14a4.002 4.002 0 013.874 3H26v2h-3.126a4.002 4.002 0 01-7.748 0H0v-2h15.126c.444-1.725 2.01-3 3.874-3zm0 2a2 2 0 10-.001 3.999A2 2 0 0019 16zM9 7a4.002 4.002 0 013.874 3H26v2H12.874a4.002 4.002 0 01-7.748 0H0v-2h5.126C5.57 8.275 7.136 7 9 7zm0 2a2 2 0 10-.001 3.999A2 2 0 009 9zm6-9a4.002 4.002 0 013.874 3H26v2h-7.126a4.002 4.002 0 01-7.748 0H0V3h11.126c.444-1.725 2.01-3 3.874-3zm0 2a2 2 0 10-.001 3.999A2 2 0 0015 2z"></path></svg>
-                                        </span>
-                                        <span className="action__label">
-                                            Search and filter
-                                        </span>
-                                    </button>
-                                    <a href="/apm/api/tas/appointmentListingCsv?terminalId=ea5a1241-9736-4b82-b815-eae337ef1ac6&amp;fileName=appointment-listing-20210812Z%24.csv" className="action action--secondary action--reverse generic-table__action generic-table__action--desktop">
-                                        <span className="action__icon">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 18" width="14" height="18"><path d="M10.664 0H0v18h14V3.454zm-.849 2L12 4.263V16H2V2zM3 10h3v2H3zm0-3h8v2H3zm4 3h4v2H7zm-4 3h3v2H3zm4 0h4v2H7z" fill="#ff6441"></path></svg>
-                                        </span>
-                                        <span className="action__label">
-                                            Export to CSV
-                                        </span>
-                                    </a>
-                                </div>
-                            </div>
-
                             <table className="fixed-table__data-table generic-table__table">
                                 <thead className="generic-table__thead">
                                     <tr className="generic-table__row">
@@ -124,7 +118,7 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
                                 <tbody className="generic-table__tbody">
                             
                                 {orders.map((order, i) => {
-                                    const hasMultipleLineItems = order.LineItems.length > 1;
+                                    const hasMultipleLineItems = order.LineItems?.length > 1;
                                     let liOrders;
                                     if (hasMultipleLineItems) {
                                         liOrders = order.LineItems.map((li, liIndex) => {
@@ -147,7 +141,7 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
                                         hasMultipleLineItems ? (
                                             liOrders.map((li, ii) => {
                                                 return (
-                                                    <tr key={ii} className={`generic-table__row generic-table__row--${(i + 1) % 2 ? 'odd' : 'even'}`}>
+                                                    <tr key={ii} className={`generic-table__row generic-table__row--${(i + 1) % 2 ? 'odd' : 'even'}`} data-index={i}>
                                                         <td className="generic-table__cell">{li.username}</td>
                                                         <td className="generic-table__cell">{li.fromCompanyID}</td>
                                                         <td className="generic-table__cell">{li.dateSubmitted}</td>
@@ -162,13 +156,23 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
                                                         {(order.Status !== 'Completed' && order.Status !== 'Canceled') && (
                                                             <td className="generic-table__cell generic-table__cell--flex">
                                                                 <button className="action generic-table__action generic-table__action--desktop generic-table__action--filter action--secondary" data-orderid={order.ID} onClick={completeOrder}>
-                                                                    <span className="action__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 18" width="14" height="18"><path d="M10.664 0H0v18h14V3.454zm-.849 2L12 4.263V16H2V2zM3 10h3v2H3zm0-3h8v2H3zm4 3h4v2H7zm-4 3h3v2H3zm4 0h4v2H7z" fill="#ff6441"></path></svg></span>
+                                                                    <span className="action__loader">
+                                                                        <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+                                                                            <circle cx="70" cy="28" r="14" transform="translate(-55 -13)" stroke="#FFE0D9" stroke-width="2" fill="none" fill-rule="evenodd" pathLength="100"></circle>
+                                                                        </svg>
+                                                                    </span>
+                                                                    <span className="action__icon action__icon--complete"></span>
                                                                     <span className="action__label">Complete</span>
                                                                 </button>
 
                                                                 {order?.xp?.RequestToCancel && 
                                                                 <button className="action generic-table__action generic-table__action--desktop generic-table__action--filter action--secondary" data-orderid={order.ID} onClick={cancelOrder}>
-                                                                    <span className="action__icon"><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M13.824 5 15 6.176 11.176 10 15 13.824 13.824 15 10 11.176 6.176 15 5 13.824 8.824 10 5 6.176 6.176 5 10 8.823 13.824 5z" fill="#3C3C46" fillRule="evenodd"></path></svg></span>
+                                                                    <span className="action__loader">
+                                                                        <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+                                                                            <circle cx="70" cy="28" r="14" transform="translate(-55 -13)" stroke="#FFE0D9" stroke-width="2" fill="none" fill-rule="evenodd" pathLength="100"></circle>
+                                                                        </svg>
+                                                                    </span>
+                                                                    <span className="action__icon"><svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" style={{marginRight: "1px", marginBottom: "1px"}}><path d="M13.824 5 15 6.176 11.176 10 15 13.824 13.824 15 10 11.176 6.176 15 5 13.824 8.824 10 5 6.176 6.176 5 10 8.823 13.824 5z" fill="#3C3C46" fillRule="evenodd"></path></svg></span>
                                                                     <span className="action__label">Cancel</span>
                                                                 </button>}
                                                             </td>
@@ -178,7 +182,7 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
                                                 );
                                             })
                                         ) : (
-                                            <tr key={i} className={`generic-table__row generic-table__row--${(i + 1) % 2 ? 'odd' : 'even'}`}>
+                                            <tr key={i} className={`generic-table__row generic-table__row--${(i + 1) % 2 ? 'odd' : 'even'}`} data-index={i}>
                                                 <td className="generic-table__cell">{order.FromUser.Username}</td>
                                                 <td className="generic-table__cell">{order.FromCompanyID}</td>
                                                 <td className="generic-table__cell">{order.DateSubmitted}</td>
@@ -199,15 +203,23 @@ const AppointmentListingPage: FunctionComponent<OcProductListProps> = () => {
                                                 {(order.Status !== 'Completed' && order.Status !== 'Canceled') && (
                                                     <td className="generic-table__cell generic-table__cell--flex">
                                                         <button className="action generic-table__action generic-table__action--desktop generic-table__action--filter action--secondary" data-orderid={order.ID} onClick={completeOrder}>
-                                                            <span className="action__icon">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 18" width="14" height="18"><path d="M10.664 0H0v18h14V3.454zm-.849 2L12 4.263V16H2V2zM3 10h3v2H3zm0-3h8v2H3zm4 3h4v2H7zm-4 3h3v2H3zm4 0h4v2H7z" fill="#ff6441"></path></svg>
+                                                            <span className="action__loader">
+                                                                <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+                                                                    <circle cx="70" cy="28" r="14" transform="translate(-55 -13)" stroke="#FFE0D9" strokeWidth="2" fill="none" fillRule="evenodd" pathLength="100"></circle>
+                                                                </svg>
                                                             </span>
+                                                            <span className="action__icon action__icon--complete"></span>
                                                             <span className="action__label">Complete</span>
                                                         </button>
 
                                                         {order?.xp?.RequestToCancel && 
                                                         <button className="action generic-table__action generic-table__action--desktop generic-table__action--filter action--secondary" data-orderid={order.ID} onClick={cancelOrder}>
-                                                            <span className="action__icon"><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M13.824 5 15 6.176 11.176 10 15 13.824 13.824 15 10 11.176 6.176 15 5 13.824 8.824 10 5 6.176 6.176 5 10 8.823 13.824 5z" fill="#3C3C46" fillRule="evenodd"></path></svg></span>
+                                                            <span className="action__loader">
+                                                                <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+                                                                    <circle cx="70" cy="28" r="14" transform="translate(-55 -13)" stroke="#FFE0D9" strokeWidth="2" fill="none" fillRule="evenodd" pathLength="100"></circle>
+                                                                </svg>
+                                                            </span>
+                                                            <span className="action__icon"><svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" style={{marginRight: "1px", marginBottom: "1px"}}><path d="M13.824 5 15 6.176 11.176 10 15 13.824 13.824 15 10 11.176 6.176 15 5 13.824 8.824 10 5 6.176 6.176 5 10 8.823 13.824 5z" fill="#3C3C46" fillRule="evenodd"></path></svg></span>
                                                             <span className="action__label">Cancel</span>
                                                         </button>}
                                                     </td>
